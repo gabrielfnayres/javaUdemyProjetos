520784f9ad06c2a60b1c0d481439ec8b9fb2afe7
Gabriel <93880679+gabrielfnayres@users.noreply.github.com>
2023-02-24 23:48:41 -0300

    Add files via upload

---
+++ aulaEntradaDeDados/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.Locale;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String x;
+        int y;
+        double z;
+        char w;
+
+
+        x = sc.next();
+        y = sc.nextInt();
+        z = sc.nextDouble();
+        w = sc.next().charAt(0);
+
+
+
+        System.out.println(x);
+
+
+
+        System.out.println(y);
+
+
+
+
+        System.out.println(z);
+
+
+
+        System.out.println(w);
+
+        sc.close();
+
+    }
+}
