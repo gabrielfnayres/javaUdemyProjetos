4632f5ab6fbf515e88f02f5f659e5202e44c7c80
Gabriel <93880679+gabrielfnayres@users.noreply.github.com>
2023-06-20 00:00:16 -0300

    Add files via upload

---
+++ estruturaFor6/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+
+        for (int i =1; i <=n; i++) {
+            if (n % i == 0) {
+                System.out.println(i);
+            }
+
+        }
+        input.close();
+
+    }
+}
