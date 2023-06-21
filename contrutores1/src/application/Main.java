82a75cabaa0329757c0a979d084dd2dde3405bc6
Gabriel <93880679+gabrielfnayres@users.noreply.github.com>
2023-06-21 19:06:07 -0300

    Add files via upload

---
+++ contrutores1/src/application/Main.java
@@ -0,0 +1,39 @@
+package application;
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Product;
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        String name = input.nextLine();
+        System.out.print("Price: ");
+        double price = input.nextDouble();
+        System.out.print("Quantity in stock: ");
+        int quantity = input.nextInt();
+
+        Product product = new Product(name, price, quantity);
+
+        System.out.println();
+        System.out.println("Product data: " + product);
+        System.out.println();
+        System.out.print("Enter the number of products to be added in stock: ");
+        quantity = input.nextInt();
+        product.addProducts(quantity);
+        System.out.println();
+        System.out.println("Updated data: " + product);
+        System.out.println();
+        System.out.print("Enter the number of products to be removed from stock: ");
+        quantity = input.nextInt();
+        product.removeProducts(quantity);
+        System.out.println();
+        System.out.println("Updated data: " + product);
+
+
+        input.close();
+
+        }
+    }
